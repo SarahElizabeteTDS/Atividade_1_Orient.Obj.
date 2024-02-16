8acfae94664418ac46a8060b31ef30a5edb2813e
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2024-02-16 14:01:22 -0300

    Update AtividadeMultiplicacao.php

--- AtividadeMultiplicacao.php
+++ AtividadeMultiplicacao.php
@@ -3,12 +3,12 @@
     $num1 = readline("Insira o primeiro número a ser incluido na operação: ");
     $num2 = readline("Insira o segundo número a ser incluido na operação: ");
 
-    function multiplicacao($num1, $num2)
+    function multiplicacao($n1, $n2)
     {
         $result = 0;
-        for ($i = 0; $i < $num2; $i++) 
+        for ($i = 0; $i < $n2; $i++) 
         { 
-            $result += $num1;
+            $result += $n1;
         }
 
         return $result;
